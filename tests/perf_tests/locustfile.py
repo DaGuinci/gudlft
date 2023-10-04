222d3ba7e24725eebdd93f30be1502d3b1d90718
DaGuinci <daguinci@mailo.com>
2023-10-04 11:52:01 +0200

    test(locustfile.py): add client actions

--- tests/perf_tests/locustfile.py
+++ tests/perf_tests/locustfile.py
@@ -2,10 +2,32 @@ from locust import HttpUser, task
 
 
 class ProjectPerfTest(HttpUser):
-    @task()
+    # A secretary visit the login page
+    @task
     def home(self):
         self.client.get("/")
 
-    @task()
+    # A secretary logs in
+    @task
+    def loginself(self):
+        self.client.post(
+            "/showSummary",
+            data={'email': 'john@simplylift.co'}
+        )
+
+    # A secretary get the board page
+    @task
     def board(self):
         self.client.get("/board")
+
+    # A secretary purchase places
+    @task
+    def purchase_places(self):
+        self.client.post(
+            "/purchasePlaces",
+            data={
+                'club': 'Simply Lift',
+                'competition': 'Spring Festival',
+                'places': 4
+            },
+            )
