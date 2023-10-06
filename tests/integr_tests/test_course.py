5c3c5f12925c1230ec03e8296a2cf72782c09f61
DaGuinci <daguinci@mailo.com>
2023-10-06 12:21:29 +0200

    test(test.course.py): test an user's journey

--- tests/integr_tests/test_course.py
+++ tests/integr_tests/test_course.py
@@ -18,7 +18,7 @@ def test_login_purchase_get_board_logout(client):
     assert 'Welcome, test@clubtest.com' in response.data.decode()
 
     # he can see his available points
-    initial_points = get_points_from_summary(soup) #  13
+    initial_points = get_points_from_summary(soup)  # 13
     assert initial_points == 13
 
     # he visits the purchase places page
@@ -38,6 +38,19 @@ def test_login_purchase_get_board_logout(client):
     assert 'Great-booking complete!' in response.data.decode()
 
     # he wants to see the available points board
-    print('+++++++++++++++++++++++++')
-    print(response.data.decode())
-    # print(html.unescape(response.data.decode()))
+    soup = BeautifulSoup(response.data, 'html.parser')
+    board_link = soup.find_all('a')[-1].get('href')
+    response = client.get(board_link)
+    assert '<h3>Table of club\'s points:</h3>' in response.data.decode()
+
+    # to log out, he goes back
+    response = client.post(
+        "/showSummary",
+        data={'email': 'test@clubtest.com'},
+        follow_redirects=True
+        )
+    soup = BeautifulSoup(response.data, 'html.parser')
+    logout_link = soup.find('a').get('href')
+    response = client.get(logout_link, follow_redirects=True)
+    soup = BeautifulSoup(response.data, 'html.parser')
+    assert 'GUDLFT Registration' == soup.title.text
